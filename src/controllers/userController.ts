04f682d7193269cf2104155c3f0b3f158f9b9c65
Patrick Smith <patrick.smith@kashew.net>
2021-04-23 17:01:12 -0400

    Initial Winston Logger

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express'
 import { User } from '../models'
+import logger from '../utils/logger'
 
 const index = async function(_req: Request, res: Response): Promise<void> {
   const users = await User.findAll()
+  logger.info(users)
 
   res.send(users)
 }
